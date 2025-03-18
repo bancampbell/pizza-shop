091a1865f626ab22950319ac5f829085202135ba
Sergey Gavrilov <jmaster@mail.ru>
2025-03-18 22:08:35 +0400

    Добавлен сидер товаров

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+
+        // Создание тестовых продуктов
+        $products = [
+            [
+                'name' => 'Пепперони',
+                'description' => 'Пицца с пепперони и сыром',
+                'price' => 499,
+                'type' => 'pizza',
+            ],
+            [
+                'name' => 'Маргарита',
+                'description' => 'Классическая пицца с томатами и моцареллой',
+                'price' => 599,
+                'type' => 'pizza',
+            ],
+            [
+                'name' => 'Гавайская',
+                'description' => 'Пицца с ветчиной и ананасами',
+                'price' => 799,
+                'type' => 'pizza',
+            ],
+            [
+                'name' => 'Спрайт',
+                'description' => 'Газированный напиток',
+                'price' => 79,
+                'type' => 'drink',
+            ],
+            [
+                'name' => 'Сок',
+                'description' => 'Ананасовый сок',
+                'price' => 69,
+                'type' => 'drink',
+            ],
+        ];
+
+        // Добавление продуктов в базу данных
+        foreach ($products as $product) {
+            Product::create($product);
+        }
+
+    }
+
+}
